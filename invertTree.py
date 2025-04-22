dfd01e8c86c47a912b870d943a251c414bfc8482
mu9326 <mu9326@g.rit.edu>
2025-04-22 16:57:06 -0400

    Invert Binary Tree

---
+++ invertTree.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+from collections import deque
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def invertTreeDFS(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+    from collections import deque
+
+    def invertTreeBFS(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None  # If the tree is empty, return None
+
+        queue = deque([root])  # Initialize the queue with the root node
+
+        while queue:
+            current = queue.popleft()  # Dequeue the next node
+
+            # Swap the left and right children
+            current.left, current.right = current.right, current.left
+
+            # Enqueue the children to process them in the next levels
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+
+        return root  # Return the root of the inverted tree
