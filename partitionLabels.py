355201db876d6c6bddb41a7eb0d90148be154a32
mu9326 <mu9326@g.rit.edu>
2025-10-28 20:51:35 -0700

    Solved Partition Labels

---
+++ partitionLabels.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        # ababcbacadefegdehijhklij
+        # size = 7, end = 15
+
+        size = 0
+        end = 0
+        res = []
+        # res = [9]
+
+        # first pass
+        # populate the hashmap with char : lastIndex
+        last_index = {}
+        for i, c in enumerate(s):
+            last_index[c] = i
+
+        # iterate over the string:
+        for i in range(len(s)):
+            # check the lastIdex of current char
+            if last_index[s[i]] > end:
+                # if it is greater than the end, update the end
+                end = last_index[s[i]]
+
+            # update the size
+            size += 1
+
+            # if size + res[-1] > end:
+            if i == end:
+                # append size to result
+                res.append(size)
+                # reset the size
+
+                size = 0
+        return res
